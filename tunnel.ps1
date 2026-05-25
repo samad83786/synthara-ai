c3479f575d142a9e5b19eb167e3ac9819e74687c
abdul <a.samad@snsonline.co.uk>
2026-05-25 01:01:26 +0100

    Remove source code. Add self-contained install.ps1 with embedded code.

--- tunnel.ps1
+++
@@ -1,6 +0,0 @@
-$psi = New-Object System.Diagnostics.ProcessStartInfo
-$psi.FileName = "ssh.exe"
-$psi.Arguments = "-o StrictHostKeyChecking=no -o ServerAliveInterval=30 -R 80:localhost:8000 nokey@localhost.run"
-$psi.UseShellExecute = $false
-$psi.CreateNoWindow = $true
-[System.Diagnostics.Process]::Start($psi) | Out-Null
